47c85b534ddf757bd68922f72ad53eb80502643f
Ian Bell <ian.h.bell@gmail.com>
2016-05-20 11:03:46 +0200

    Finish calculation of activity coefficients (#1)
    * Setting of components mostly working
    
    * Finish loading of UNIFAC data from file
    
    * Get to point of calculating combinatorial contribution to AC
    
    All terms that are only a function of composition are calculated and validated against Poling example
    
    * Implemented remaining terms that are function of composition but not temperature
    
    * Calculate the rest of the stuff for the pures
    
    * UNIFAQ is working! Can reproduce Poling example from textbook
    
    * Update the jupyter notebook
    
    * Add two examples and fix the resetting of variables when doing multiple calculations

--- examples/speed_test.cpp
+++ examples/speed_test.cpp
@@ -4,28 +4,35 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
+#include <time.h>
 
 int main()
 {
     std::ifstream t1("../dev/Horstmann_group_data.json"), t2("../dev/Horstmann_interaction_parameters.json"), t3("../dev/Kang_decomps.json");
     std::stringstream b1, b2, b3;
     b1 << t1.rdbuf(); b2 << t2.rdbuf(); b3 << t3.rdbuf();
+    double rrrr = 0;
     {
         using namespace UNIFAQLibrary;
-        
+
         UNIFAQParameterLibrary lib;
         lib.populate(b1.str(), b2.str(), b3.str());
-        
-        UNIFAQ::UNIFAQMixture mix(lib);
-        std::vector<std::string> names(2, "Acetone"); names[1] = "n-Pentane"; 
-        mix.set_components("name", names);
 
+        double t1 = clock();
+        long N = 10000;
+        UNIFAQ::UNIFAQMixture mix(lib);
+        std::vector<std::string> names(2, "Acetone"); names[1] = "n-Pentane";
+        mix.set_components("name", names);
         mix.set_interaction_parameters();
-        std::vector<double> z(2,0.047); z[1] = 1-z[0];
-        mix.set_mole_fractions(z);
-        mix.set_temperature(307);
-        for (std::size_t i = 0; i < 2; ++i){
-            printf("gamma_{%d}: %g\n", i, mix.activity_coefficient(i));
+
+        for (int ii = 0; ii < N; ++ii) {
+            std::vector<double> z(2, 0.047); z[1] = 1 - z[0];
+            mix.set_mole_fractions(z);
+            mix.set_temperature(307);
+            rrrr += mix.activity_coefficient(0);
         }
+        double t2 = clock();
+        std::cout << rrrr/N << " " << (t2 - t1) / ((double)CLOCKS_PER_SEC) / ((double)N)*1e6 << std::endl;
     }
 }
--- src/UNIFAQLibrary.cpp
+++ src/UNIFAQLibrary.cpp
@@ -13,7 +13,7 @@ namespace UNIFAQLibrary{
             return d;
         }
     }
-    void UNIFAQParameterLibrary::populate(rapidjson::Value &group_data, rapidjson::Value &interaction_data)
+    void UNIFAQParameterLibrary::populate(rapidjson::Value &group_data, rapidjson::Value &interaction_data, rapidjson::Value &comp_data)
     {
         // Schema should have been used to validate the data already, so by this point we are can safely consume the data without checking ...
         for (rapidjson::Value::ValueIterator itr = group_data.Begin(); itr != group_data.End(); ++itr)
@@ -38,12 +38,35 @@ namespace UNIFAQLibrary{
             ip.c_ji = (*itr)["c_ji"].GetDouble();
             interaction_parameters.push_back(ip);
         }
+        for (rapidjson::Value::ValueIterator itr = comp_data.Begin(); itr != comp_data.End(); ++itr)
+        {
+            Component c;
+            c.inchikey = (*itr)["inchikey"].GetString();
+            c.registry_number = (*itr)["registry_number"].GetString();
+            c.name = (*itr)["name"].GetString();
+            // userid is an optional user identifier
+            if ((*itr).HasMember("userid")){
+                c.userid = (*itr)["userid"].GetString();
+            }
+            rapidjson::Value &groups = (*itr)["groups"];
+            for (rapidjson::Value::ValueIterator itrg = groups.Begin(); itrg != groups.End(); ++itrg)
+            {
+                int count = (*itrg)["count"].GetInt();
+                int sgi = (*itrg)["sgi"].GetInt();
+                if  (has_group(sgi)){
+                    ComponentGroup cg(count, get_group(sgi));
+                    c.groups.push_back(cg);
+                }
+            }
+            components.push_back(c);
+        }
     }
-    void UNIFAQParameterLibrary::populate(std::string &group_data, std::string &interaction_data)
+    void UNIFAQParameterLibrary::populate(std::string &group_data, std::string &interaction_data, std::string &decomp_data)
     {
         rapidjson::Document group_JSON = jsonize(group_data);
         rapidjson::Document interaction_JSON = jsonize(interaction_data);
-        populate(group_JSON, interaction_JSON);
+        rapidjson::Document decomp_JSON = jsonize(decomp_data);
+        populate(group_JSON, interaction_JSON, decomp_JSON);
     }
     Group UNIFAQParameterLibrary::get_group(int sgi) const {
         for (std::vector<Group>::const_iterator it = groups.cbegin(); it != groups.cend(); ++it) {
@@ -51,7 +74,21 @@ namespace UNIFAQLibrary{
         }
         throw std::exception("Could not find group");
     }
+    bool UNIFAQParameterLibrary::has_group(int sgi) const {
+        for (std::vector<Group>::const_iterator it = groups.cbegin(); it != groups.cend(); ++it) {
+            if (it->sgi == sgi) { return true; }
+        }
+        return false;
+    }
+
     InteractionParameters UNIFAQParameterLibrary::get_interaction_parameters(int mgi1, int mgi2) const {
+
+        // If both mgi are the same, yield all zeros for the interaction parameters
+        if (mgi1 == mgi2){
+            InteractionParameters ip; ip.mgi1 = mgi1; ip.mgi2 = mgi2;
+            ip.zero_out();
+            return ip;
+        }
         for (std::vector<InteractionParameters>::const_iterator it = interaction_parameters.cbegin(); it != interaction_parameters.cend(); ++it) {
             if (it->mgi1 == mgi1 && it->mgi2 == mgi2) {
                 // Correct order, return it
@@ -65,6 +102,15 @@ namespace UNIFAQLibrary{
             }
         }
         throw std::exception("Could not find interaction pair");
+    }
+
+    Component UNIFAQParameterLibrary::get_component(const std::string &identifier, const std::string &value) const {
+        if (identifier == "name"){
+            for (std::vector<Component>::const_iterator it = components.cbegin(); it != components.cend(); ++it ){
+                if (it->name == value ){ return *it; }
+            }
+        }
+        throw std::exception("Could not find component");
     }
 
 }; /* namespace UNIFAQLibrary */
